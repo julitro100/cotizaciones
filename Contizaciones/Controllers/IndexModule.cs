241bd3ce345ef8d29c25c2a496d9bb2473094e43
clopez@apci.gob.pe <clopez@apci.gob.pe>
2015-10-22 14:19:02 -0500

    Version Inicial

---
+++ Contizaciones/Controllers/IndexModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contizaciones.Controllers
+{
+    public class IndexModule : Nancy.NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = index;
+        }
+
+        private dynamic index(dynamic args)
+        {
+            return View["index"];
+        }
+    }
+}
