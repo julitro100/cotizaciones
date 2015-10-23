75bee6a089801240f7ec3bc802e3ff2f62bed4ac
clopez@apci.gob.pe <clopez@apci.gob.pe>
2015-10-23 08:45:10 -0500

    --reset--

--- Contizaciones/Content/app/config/config.js
+++
@@ -1,9 +0,0 @@
-angular.module("main").config(['$routeProvider',function($routeProvider){
-
-	$routeProvider.
-
-		when('/',  			{ templateUrl: 'app/views/contacto.jsp' 	}).
-		//when('/registro',  	{ templateUrl: 'app/views/registrocotizacion.jsp' 	}).
-		otherwise({	redirectTo: '/'	});
-
-}]);
